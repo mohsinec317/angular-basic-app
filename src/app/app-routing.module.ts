9da5d9c391145e47e772874ce15bfac0562182d2
unknown <mohsinec317@gmail.com>
2021-03-01 21:51:30 +0530

    Help with inner page redirections without home page and mobile view on scroll doesnt go till the end

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+
+const routes: Routes = [
+  {
+    path: 'app-details', component: DetailsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+
+
+const appRoutes: Routes = [
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
